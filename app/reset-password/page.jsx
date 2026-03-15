0fd68ee02957a94ad98348f272708b4d477e9d35
karoobuks <karoobuks@gmail.com>
2026-03-15 22:16:15 +0100

    color pallete regulated

--- app/reset-password/page.jsx
+++ app/reset-password/page.jsx
@@ -76,7 +76,7 @@ function ResetPasswordFormComponent() {
         <section className='bg-gray-50'>
             <div className='container m-auto max-w-2xl py-24'>
                 <div className='bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
-                    <h2 className='text-3xl text-center font-semibold mb-6'>
+                    <h2 className='text-3xl text-center font-semibold mb-6 text-blue-600'>
                         Reset Your Password
                     </h2>
                     <form onSubmit={handleSubmit}>
